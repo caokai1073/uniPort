4455cc4558161418f98238c48a1629894fb12011
caokai1073 <caokai17@mails.ucas.ac.cn>
2022-05-07 17:35:55 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,3 +44,14 @@ latex_use_xindy = False
 latex_elements = {
     'preamble': '\\usepackage[UTF8]{ctex}\n',
 }
+
+html_theme_options = dict(navigation_depth=4, logo_only=True)  # Only show the logo
+html_context = dict(
+    display_github=True,  # Integrate GitHub
+    github_user='jsxlei',  # Username
+    github_repo='SCALEX',  # Repo name
+    github_version='main',  # Version
+    conf_py_path='/docs/',  # Path in the checkout to the docs root
+)
+html_static_path = ['_static']
+html_show_sphinx = False
